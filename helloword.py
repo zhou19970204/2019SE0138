4707fe49122d02a6e8a8595b63665b5336cddbd7
zhou19970204 <48430275+zhou19970204@users.noreply.github.com>
2019-03-11 15:40:24 +0800

    作业提交

--- helloword.py
+++ helloword.py
@@ -12,5 +12,5 @@ Created on 2019年3月11日 Python 2.7
 if __name__ == '__main__':
     print('Hello, I am first python script on Github')
     print('你好，我是在Github上的第一个python程序!\n')
-    print('你好，我的学号后四位是0101。')  
+    print('你好，我的学号后四位是0138。')  
     
